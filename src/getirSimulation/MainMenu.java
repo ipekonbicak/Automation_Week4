44e16c66f1fae7bf0f39d1b85a09ce6e4dd3e621
ipek önbıçak <ipek.onbicak@getir.com>
2021-10-12 00:09:15 +0300

    listed products transaction DONE

--- src/getirSimulation/MainMenu.java
+++ src/getirSimulation/MainMenu.java
@@ -25,33 +25,28 @@ public class MainMenu {
         product.createCategoryByDefault();
     }
 
+    //list categories
     public void mainMenu(Client client){
         System.out.println("\n **************** Product Categories ****************");
-
-
-
                 /* To get the category names, I pulled the category names from the category
                 list I created in the category class.
                  */
         for (Category c: category.categoryList) {
             System.out.println(c.name);
-            for(int y = 0; y < product.productList.size(); y++){
-                if((c.name.equals(product.productList.get(y).categoryName) && product.productList.get(y).subCategory.equals("-"))){
-                    System.out.println(" - Product Name: " +product.productList.get(y).name+ "  Price: " +product.productList.get(y).price);
-                }
-            }
         }
 
         this.choose(client);
 
     }
 
+    //list subcategory belongs to related category
     public void listSubCategory(String choosen, Client client){
         for (SubCategory subCategory: subCategory.subCategoryList){
 
             /* The category name selected by the user is
             checked and the sub-categories of that category are listed.
             */
+            System.out.println("choosen " +choosen+ " cName: " +subCategory.categoryName);
             if(choosen.equals(subCategory.categoryName)){
                 System.out.println(" - " +subCategory.name);
             }
@@ -61,15 +56,26 @@ public class MainMenu {
         this.choose(client);
     }
 
-    //To add products to the Basket, view Categories again or Search.
+    //list products belongs to related subcategory
+    public void listProduct(String name){
+        for(int y = 0; y < product.productList.size(); y++){
+            if(product.productList.get(y).subCategory.equals(name)){
+                System.out.println(" - Product Name: " +product.productList.get(y).name+ "  Price: " +product.productList.get(y).price+ " Discounted Price:  " +product.productList.get(y).discountedPrice);
+            }
+        }
+    }
+
+
+    //I wanted this place to be opened after every transaction in the form of a menu.
     public void choose(Client client){
         System.out.println("\n");
         System.out.println("**** Menu *** \n" +
-                " Press 'C' to turn back to Categories \n" +
-                " Press 'S' to list SubCategories \n" +
+                " Press 'CL' to turn back to Categories \n" +
+                " Press 'SL' to list SubCategories \n" +
                 " Press 'SP' to search \n Press 'P' add product to Basket " +
                 "\n Press 'SB' to show your Basket." +
-                "\n Press 'L' to turn back to landing page");
+                "\n Press 'L' to turn back to landing page" +
+                "\n Press 'PL' to list subcategories' products.");
         choosen = scn.next();
         Scanner scanner = new Scanner(System.in);
         switch (choosen){
@@ -93,15 +99,19 @@ public class MainMenu {
                 int number = scanner.nextInt();
                 basket.addProduct(number, p, client);
                 break;
-            case "C":
+            case "CL":
                 this.mainMenu(client);
-            case "S":
+            case "SL":
                 System.out.println("\nChoose Category for seeing SubCategory");
                 choosen  = scn.next();
                 this.listSubCategory(choosen,client);
             case "L":
                 LandingPage landingPage = new LandingPage();
                 landingPage.landingPage(client);
+            case "PL":
+                System.out.println("\nChoose SubCategory for seeing Products");
+                choosen  = scn.next();
+                this.listProduct(choosen);
             default:
                 break;
 
